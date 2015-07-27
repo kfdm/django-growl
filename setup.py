cfcb66b60aaecca7b503648b278f2a85fc3f8b9f
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-07-27 15:09:29 +0900

    Initial stub

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from webgntp import __version__
+
+setup(
+    name='django-growl',
+    version=__version__,
+    packages=find_packages(exclude=['test']),
+    include_package_data=True,
+    license='MIT License',
+    description='A simple growl forwarder',
+    url='https://github.com/kfdm/django-growl',
+    author='Paul Traylor',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        'djangorestframework',
+    ],
+    entry_points={
+        'django.apps': ['growl = webgntp'],
+        'django.urls': ['growl = webgntp.urls']
+    },
+)
